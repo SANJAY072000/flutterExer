ca6d47376577eb4d777b1779a34073bb8419d789
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2021-05-31 12:19:55 +0530

    Done with helper file

--- 04/spanishaudio/lib/main.dart
+++ 04/spanishaudio/lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spanish Audio App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
